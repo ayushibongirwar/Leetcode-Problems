4a02b16a643425f284eaf670c40e9c10403ca4eb
Ayushi Bongirwar <99348209+ayushibongirwar@users.noreply.github.com>
2023-03-30 21:11:36 +0530

    Update group anagram.cpp

--- group anagram.cpp
+++ group anagram.cpp
@@ -6,15 +6,7 @@ public:
         
        unordered_map<string, vector<string>>mp;
         
-         /*
-                Consider example 1 : strs = ["eat","tea","tan","ate","nat","bat"]
-                
-                After the below opeartion of for loop map will contain
-                
-                aet -- eat, tea, ate
-                ant -- tan, nat
-                abt -- bat
-        */
+       
         
         for(int i = 0 ; i < strs.size() ; i++)
         {
@@ -23,7 +15,7 @@ public:
             mp[strs[i]].push_back(s);
         }
         
-        //now simply put the elements  of second column of map in ans
+       
         
         for(auto i : mp)
         {
